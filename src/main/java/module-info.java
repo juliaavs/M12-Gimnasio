5a6d8139c12861ffb0c4348f4d2ed1c5f6d68503
sirthiccolas <yelko.nunez@inslapineda.cat>
2025-10-09 13:47:37 +0200

    Tabla de instructores

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 module com.mycompany.proyectogimnasio {
     requires javafx.controls;
     requires javafx.fxml;
@@ -7,3 +8,16 @@ module com.mycompany.proyectogimnasio {
     opens com.mycompany.proyectogimnasio.Controllers to javafx.fxml;
     exports com.mycompany.proyectogimnasio;
 }
+=======
+module com.mycompany.proyectogimnasio {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+    requires java.desktop;
+    requires java.logging;
+    
+    opens com.mycompany.proyectogimnasio to javafx.fxml;
+    opens com.mycompany.proyectogimnasio.Controllers to javafx.fxml; // Abrir controladores
+    exports com.mycompany.proyectogimnasio;
+}
+>>>>>>> Stashed changes
